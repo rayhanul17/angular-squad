5a774f93c44c4a6daac5b3977b1032a6ee6340d4
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2024-03-22 19:55:45 +0600

    create main card

--- angularSA1/src/app/app.module.ts
+++ angularSA1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainCard } from './main-card/mainCard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCard
   ],
   imports: [
     BrowserModule,
---
+++ angularSA1/src/app/main-card/mainCard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-mainCard',
+    templateUrl:'./mainCard.component.html',
+    styleUrls:['./mainCard.component.css']
+})
+export class MainCard{
+
+}
