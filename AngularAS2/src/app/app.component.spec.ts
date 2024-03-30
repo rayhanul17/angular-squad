5ed77db2a7ffe447a3e451efa594090d0c33c38d
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2024-03-30 11:39:09 +0600

    add nav-bar, home page and footer

--- AngularAS2/src/app/app.component.spec.ts
+++ AngularAS2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DefaultAngularApp'`, () => {
+  it(`should have as title 'AngularAS2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DefaultAngularApp');
+    expect(app.title).toEqual('AngularAS2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DefaultAngularApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularAS2');
   });
 });
--- AngularAS2/src/app/app.module.ts
+++ AngularAS2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
