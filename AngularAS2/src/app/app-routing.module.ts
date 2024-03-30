89081bcfd6895fe32404babeb36878db11a0905f
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2024-03-30 12:08:58 +0600

    add component, module with routing

--- AngularAS2/src/app/app-routing.module.ts
+++ AngularAS2/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UsersComponent } from './users/users.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path:'users',
+    component: UsersComponent
+  },
+  {
+    path:'course-details',
+    component: CourseDetailsComponent,
+    loadChildren: () => import('./course-details/course-details.module').then( (m) => m.CourseDetailsModule) 
+  },
+  {
+    path:'**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularAS2/src/app/app.module.ts
+++ AngularAS2/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UsersComponent } from './users/users.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { CourseDetailsModule } from './course-details/course-details.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent,
+    UsersComponent,
+    CourseDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CourseDetailsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
