123263b4f89720866d9ac8508a0b4a28fb147431
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2024-05-20 01:33:15 +0600

    move routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ const routes: Routes = [
   
   {
     path:'users',
-    loadChildren: () => import('./app/users/users.module').then((m) => m.UsersModule)
+    loadChildren: () => import('./users/users.module').then((m) => m.UsersModule)
   },
   {
     path: 'public', 
-    loadChildren: () => import('./app/public/public.module').then(m => m.PublicModule) 
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule) 
   }
 ];
 
